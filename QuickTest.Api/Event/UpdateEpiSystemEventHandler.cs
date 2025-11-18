ef0386a5b5c2d290d4f5aba21549e3121c37c5f5
AElmendo <aelmendorf234@gmail.com>
2025-11-18 09:08:18 -0500

    update UpdateEpiSystemEventHandler.cs to put MarkExternalProcessCompleteRequest

--- QuickTest.Api/Event/UpdateEpiSystemEventHandler.cs
+++ QuickTest.Api/Event/UpdateEpiSystemEventHandler.cs
@@ -4,6 +4,11 @@ using QuickTest.Data.Models.Measurements;
 
 namespace QuickTest.Api.Event;
 
+public class MarkExternalProcessCompleteRequest {
+    public required string WaferId { get; set; }
+    public required string ExternalReferenceId { get; set; }
+}
+
 public class UpdateEpiSystemEventHandler:IEventHandler<UpdateEpiSystemEvent> {
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly ILogger<UpdateEpiSystemEventHandler> _logger;
@@ -15,10 +20,16 @@ public class UpdateEpiSystemEventHandler:IEventHandler<UpdateEpiSystemEvent> {
     
     public async Task HandleAsync(UpdateEpiSystemEvent eventModel, CancellationToken ct) {
         var client=this._httpClientFactory.CreateClient("QuickTestApi");
-        client.BaseAddress=new Uri("http://localhost:34000/"); 
-        var response = await client.GetAsync($"api/process/quick-test/" +
+        client.BaseAddress=new Uri("http://localhost:34000/");
+        var req = new MarkExternalProcessCompleteRequest() {
+            WaferId = eventModel.WaferId,
+            ExternalReferenceId = eventModel.ReferenceId
+        };
+        /*var response = await client.GetAsync($"api/process/quick-test/" +
                                              $"{(eventModel.MeasurementType == MeasurementType.Initial ? "initial" : "final")}" +
-                                             $"/{eventModel.WaferId}?externalReferenceId={eventModel.ReferenceId}",ct);
+                                             $"/{eventModel.WaferId}?externalReferenceId={eventModel.ReferenceId}",ct);*/
+        var response=await client.PutAsJsonAsync($"api/process/quick-test/" +
+                                                 $"{(eventModel.MeasurementType == MeasurementType.Initial ? "initial" : "final")}/",req,ct);
         if (response.IsSuccessStatusCode) {
             this._logger.LogInformation("Sent ReferenceId for WaferId:{WaferId} RefId: {RefId}",
                 eventModel.WaferId,eventModel.ReferenceId);
